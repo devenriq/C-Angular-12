7224655c48791bf8983ebcc799758964705fd4ed
enrique palomino <octepc.97@gmail.com>
2023-01-05 17:54:22 -0500

    made the aside logic to render the text

--- src/app/pages/aside/aside.component.ts
+++ src/app/pages/aside/aside.component.ts
@@ -5,5 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './aside.component.html',
 })
 export class AsideComponent {
+  asideElements:any[]=[
+    {
+      title: 'about',
+      description:
+      `
+        I'm a web developer in course. I'm searching for a workplace where I can prove all the things I've been learning this year.
 
+        I'm a Post-Rock and Lomo Saltado lover.
+      `
+    },
+    {
+      title: 'skills',
+      description:
+      `
+      Technical: JavaScript, ReactJS, Node JS, CSS, HTML5, Git-GitHub, Figma,
+      Tailwind, Vite, Angular
+
+      Soft-skills: Resilience, Empathy, Teamwork
+      `
+    },
+    {
+      title: 'languages',
+      description:
+      `
+        English - advanced B2
+        Italian - basic level
+        French - basic level
+      `
+  }
+  ]
 }
+
